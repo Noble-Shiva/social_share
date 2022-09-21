76a7f15bd2f6760afc5af3a0a77324814eb68560
ShivaKumar Reddy L <l.shivkumarreddy@gmail.com>
2022-09-21 18:30:17 +0530

    Added Hosting

---
+++ hosting/public/precache-manifest.6d6071c44a9017f718da44bc1694317c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b482295484d0dfbb72b6458b24f12d9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2085dd0fe77a3ea00214",
+    "url": "/static/css/main.12617352.chunk.css"
+  },
+  {
+    "revision": "1d4440633b1e2e0f1ec2",
+    "url": "/static/js/2.340f3059.chunk.js"
+  },
+  {
+    "revision": "a7b9839227288d5c929ca877e2d54684",
+    "url": "/static/js/2.340f3059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2085dd0fe77a3ea00214",
+    "url": "/static/js/main.c3b7a3a6.chunk.js"
+  },
+  {
+    "revision": "aeb00af583819f9e4afc",
+    "url": "/static/js/runtime-main.266da7ef.js"
+  },
+  {
+    "revision": "51acae3a4b779bef4006b7585b572148",
+    "url": "/static/media/404.51acae3a.svg"
+  },
+  {
+    "revision": "51c10e909ddccba72be25a9759dfc6c5",
+    "url": "/static/media/cover_bg.51c10e90.jpg"
+  },
+  {
+    "revision": "f1be6d203f006e40a7a0516407f56fe5",
+    "url": "/static/media/cover_people.f1be6d20.jpg"
+  },
+  {
+    "revision": "e6e3cf9ffed2456002fc6e6391f980f2",
+    "url": "/static/media/facebook_gif.e6e3cf9f.gif"
+  },
+  {
+    "revision": "4ba3178f714eeb86d2f157f33087a26d",
+    "url": "/static/media/handle_bg_female_animated.4ba3178f.gif"
+  },
+  {
+    "revision": "65cc52df87055452de7bf34e4f27b0af",
+    "url": "/static/media/handle_bg_male_animated.65cc52df.gif"
+  },
+  {
+    "revision": "3e473d115e974bbba4b30cd01d0e027c",
+    "url": "/static/media/instagram_gif.3e473d11.gif"
+  },
+  {
+    "revision": "c580c423a1c4fc4494895f5c0502d68a",
+    "url": "/static/media/landing_bg.c580c423.svg"
+  },
+  {
+    "revision": "079bb83512cef826f0aebf1daaa1f98a",
+    "url": "/static/media/login.079bb835.svg"
+  },
+  {
+    "revision": "1cae7ebf0709c0284769e0d950e209a6",
+    "url": "/static/media/profile_bg.1cae7ebf.png"
+  },
+  {
+    "revision": "0037bb902816d9633cae2e14a79dd336",
+    "url": "/static/media/step_1.0037bb90.svg"
+  },
+  {
+    "revision": "9038b2095e64648e51ca8a3ec8a10d7b",
+    "url": "/static/media/step_2.9038b209.svg"
+  },
+  {
+    "revision": "a219d26b9a7faa37b4cc21ccd52039ae",
+    "url": "/static/media/step_3.a219d26b.svg"
+  },
+  {
+    "revision": "06f85c540ab7c0f608e947e48eeb8d48",
+    "url": "/static/media/twitter_gif.06f85c54.gif"
+  },
+  {
+    "revision": "2b9cbfb62673de060a2c37acd1b511ea",
+    "url": "/static/media/wfh_1.2b9cbfb6.svg"
+  }
+]);
